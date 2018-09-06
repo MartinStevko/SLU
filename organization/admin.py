fd07c26e97730c151f76f1fbc86edb78491eb5d9
MartinStevko <mstevko10@gmail.com>
2018-09-06 20:59:44 +0200

    Admin site registration

--- organization/admin.py
+++ organization/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 from .models import *
 
 # admin.site.register(Model)
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(FinalMember)
+admin.site.register(Teacher)
