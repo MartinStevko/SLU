f6e8ad37699c0965523361bf74f9a0f8e96f7d26
MartinStevko <mstevko10@gmail.com>
2018-09-06 14:40:53 +0200

    Base imports

--- organization/views.py
+++ organization/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.admin.views.decorators import staff_member_required
+
+from django.contrib.sessions.models import Session
+from .models import *
+
+def index(request):
+    return HttpResponse('<h1>Ide to!</h1>')
