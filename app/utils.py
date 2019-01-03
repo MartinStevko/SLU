9fd18e32da1d26159dfe0a0a08b42b0587105b86
MartinStevko <mstevko10@gmail.com>
2019-01-03 20:44:44 +0100

    Minority changes

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,3 @@
-import base64
 from uuid import uuid4
 
 
@@ -23,14 +22,15 @@ def encode(key, clear):
         key_c = key[i % len(key)]
         enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
         enc.append(enc_c)
-    return base64.urlsafe_b64encode(bytearray("".join(enc), 'utf-8'))
+
+    return "".join(enc)
 
 
 def decode(key, enc):
     dec = []
-    enc = base64.urlsafe_b64decode(enc).decode()
     for i in range(len(enc)):
         key_c = key[i % len(key)]
         dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
         dec.append(dec_c)
+
     return "".join(dec)
