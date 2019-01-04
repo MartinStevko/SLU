ed32e30f85263569ef985ce5f32873772bbd9658
MartinStevko <mstevko10@gmail.com>
2019-01-04 17:06:41 +0100

    SLU context processors

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,4 +1,14 @@
 from datetime import datetime
+from tournament.models import Season
+
 
 def variables_processor(request):
-    return {'outdoor': True}
+    r = {}
+
+    season = Season.objects.all().order_by('-pk')[0]
+    if season.season == 'indoor':
+        r.update({'indoor': True})
+    else:
+        r.update({'outdoor': True})
+
+    return r
