9fe957ddc073326da534e9650d04874e6b3dc95b
MartinStevko <mstevko10@gmail.com>
2019-02-23 01:52:55 +0100

    Set up debug

--- app/context_processors.py
+++ app/context_processors.py
@@ -5,7 +5,12 @@ from tournament.models import Season
 def variables_processor(request):
     r = {}
 
-    season = Season.objects.all().order_by('-pk')[0]
+    season = Season.objects.all().order_by('-pk')
+    if season:
+        season = season[0]
+    else:
+        r.update({'outdoor': True})
+
     if season.season == 'indoor':
         r.update({'indoor': True})
     else:
