28b164e152e85641e5a826b979000f0fa7a1f821
MartinStevko <mstevko10@gmail.com>
2019-11-09 01:15:59 +0100

    Error handlers

--- app/settings.py
+++ app/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
